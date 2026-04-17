6c64775689ef6fa84ff048cfd5f95d13a7d88f76
Deepak Menan R <92635140+Deepak-Menan-R@users.noreply.github.com>
2026-04-17 15:51:53 +0530

    Update app_name in Settings class

--- code_snippets/app/core/config.py
+++ code_snippets/app/core/config.py
@@ -1,7 +1,7 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    app_name: str = "FastAPI Health Service"
+    app_name: str = "FastAPI Health Service point"
     debug: bool = True
 
 settings = Settings()
