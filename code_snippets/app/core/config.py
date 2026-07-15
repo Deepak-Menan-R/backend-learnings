6823f6233273af1ed5d7dcf17aa501e6d8fdb752
Deepak Menan R <92635140+Deepak-Menan-R@users.noreply.github.com>
2026-07-15 23:36:00 +0530

    Add OpenAPI_KEY to Settings class

--- code_snippets/app/core/config.py
+++ code_snippets/app/core/config.py
@@ -3,5 +3,6 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     app_name: str = "FastAPI Health Service point"
     debug: bool = True
+    OpenAPI_KEY = "sk-..."
 
 settings = Settings()
